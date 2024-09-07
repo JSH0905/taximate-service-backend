0e7d4097489c303508364472c6ea61aba7703849
Jeong SeongHun <2000tjdgns@naver.com>
2024-09-08 04:50:34 +0900

    feat:초기 설정

---
+++ src/main/java/taximate/taximate_service/TaximateServiceApplication.java
@@ -0,0 +1,13 @@
+package taximate.taximate_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaximateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaximateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taximate/taximate_service/TaximateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package taximate.taximate_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaximateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
